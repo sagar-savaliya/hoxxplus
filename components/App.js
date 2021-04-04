f37c5f3ce7b238129e44975b999aff506a390e2a
Sagar Savaliya <sagarsavaliya407@gmail.com>
2021-04-04 16:28:08 +0530

    reformat react components

---
+++ components/App.js
@@ -0,0 +1,27 @@
+import type {Node} from 'react';
+import React from 'react';
+import {SafeAreaView, ScrollView, StatusBar, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Main from './Main';
+
+const App: () => Node = () => {
+    const isDarkMode = useColorScheme() === 'dark';
+
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
+
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}/>
+            <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                style={backgroundStyle}
+            />
+            <Main/>
+        </SafeAreaView>
+    );
+};
+
+export default App;
